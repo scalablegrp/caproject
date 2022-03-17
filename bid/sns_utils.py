3c393a3fa1ff0d7736c63be246643e9617af7f57
Your Name <djjtynan@gmail.com>
2022-03-17 16:59:24 +0000

    Customised messages publishing for sns topics

--- bid/sns_utils.py
+++ bid/sns_utils.py
@@ -13,11 +13,27 @@ class sns:
         try:
             sns_client = boto3.client('sns', region_name=env_variables.get_aws_region("aws_educate"), aws_access_key_id=env_variables.get_aws_access_key("aws_educate"), 
                                         aws_secret_access_key=env_variables.get_aws_secret_key("aws_educate"), 
-                                        aws_session_token="FwoGZXIvYXdzEAIaDLZmCOn1/5yAEQRQciLLAeJc1xErMTQMJOnnMib/Nh424cUjq1nM5abgy+InX0aaIzBg0tG4843vcCB7DnM+CjuIzOCrs52ufT0idBvqibGxyexAG7bGdDyMJLdS6/Ef1/+NslUEhWbhVkcMfjDA9xZxl46aLIORZG46pPQNu1rkc7MSDcvGnLSHY2TK99p3dQ0uAypPuQZu0YiPNHtzHors+soXn3s+i6+P7blnec0135NHEHZBv1iPgDCj8ceGL0TwqwSpd5kE7+onz5BgcP+dteQH4KDh141cKLizv5EGMi1LO1noebm7CQzXCtZrq8yhUHIFGVi5JRQ4Xo1ZLIqH8SgNSeoyVJPFdbGEfdU=")
+                                        aws_session_token=env_variables.get_aws_session_token("aws_educate"))
             response = sns_client.create_topic(Name=topic_name)
             topic_arn = response['TopicArn']
             sns_client.subscribe(TopicArn=topic_arn, Protocol = 'email', Endpoint = user_email)
         except ClientError as e:
             logging.error(e)
             print("An isue occured subscribing")
-            return
+
+    @staticmethod
+    def publish_new_bid(topic_name, message):
+        try:
+            sns_client = boto3.client('sns', region_name=env_variables.get_aws_region("aws_educate"), aws_access_key_id=env_variables.get_aws_access_key("aws_educate"), 
+                                            aws_secret_access_key=env_variables.get_aws_secret_key("aws_educate"), 
+                                            aws_session_token=env_variables.get_aws_session_token("aws_educate"))
+            # Try to publish the message
+            response = sns_client.create_topic(Name = topic_name)
+            topic_arn = response['TopicArn']
+            try:
+                response = sns_client.publish(TopicArn = topic_arn, Message = message)
+            except Exception as e:
+                print(e)
+        except ClientError as e:
+            logging.error(e)
+            print("An isue occured in the message publish process")
