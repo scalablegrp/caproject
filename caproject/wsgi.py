91a0310004c4ecf6833ca5f21941ff1fc92c70c8
94bharathraj <40537401+94bharathraj@users.noreply.github.com>
2022-04-17 22:00:43 +0100

    Update wsgi.py

--- caproject/wsgi.py
+++ caproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'caproject.settings')
 
 application = get_wsgi_application()
